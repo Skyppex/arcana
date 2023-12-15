c08b9651c3eb7374add7d4a5763c0b86216979b2
Skypex <84551201+Skyppex@users.noreply.github.com>
2023-12-16 00:59:56 +0100

    remove everything and init rust project

--- Arcana/Models/UnionMember.cs
+++
@@ -1,9 +0,0 @@
-namespace Arcana.Models;
-
-public readonly struct UnionMember(string typeName, string memberName)
-{
-    public string TypeName { get; } = typeName;
-    public string MemberName { get; } = memberName;
-
-    public override string ToString() => $"{TypeName}.{MemberName}";
-}
