c059adc2aa716905e84c1e6371b89508db643b03
Skypex <84551201+Skyppex@users.noreply.github.com>
2023-12-16 02:09:58 +0100

    add cursor and some tokens

---
+++ src/shared/src/lexer/cursor.rs
@@ -0,0 +1,53 @@
+use std::str::Chars;
+
+pub struct Cursor<'a> {
+    chars: Chars<'a>,
+    length_remaining: usize,
+}
+
+pub(crate) const EOF_CHAR: char = '\0';
+
+impl<'a> Cursor<'a> {
+    pub fn new(input: &'a str) -> Cursor<'a> {
+        Cursor {
+            length_remaining: input.len(),
+            chars: input.chars(),
+        }
+    }
+
+    pub fn as_str(&self) -> &'a str {
+        self.chars.as_str()
+    }
+
+    pub(crate) fn first(&self) -> char {
+        self.chars.clone().next().unwrap_or(EOF_CHAR)
+    }
+
+    pub(crate) fn second(&self) -> char {
+        let mut iter = self.chars.clone();
+        iter.next();
+        iter.next().unwrap_or(EOF_CHAR)
+    }
+
+    pub(crate) fn is_eof(&self) -> bool {
+        self.chars.as_str().is_empty()
+    }
+
+    pub(crate) fn pos_within_token(&self) -> u32 {
+        (self.length_remaining - self.chars.as_str().len()) as u32
+    }
+
+    pub(crate) fn reset_pos_within_token(&mut self) {
+        self.length_remaining = self.chars.as_str().len();
+    }
+
+    pub(crate) fn bump(&mut self) -> Option<char> {
+        Some(self.chars.next()?)
+    }
+
+    pub(crate) fn eat_while(&mut self, mut predicate: impl FnMut(char) -> bool) {
+        while predicate(self.first()) && !self.is_eof() {
+            self.bump();
+        }
+    }
+}
--- src/shared/src/lexer/token.rs
+++ src/shared/src/lexer/token.rs
@@ -1,5 +1,63 @@
-
-
 pub enum Token {
+    UnaryOp(UnaryOp),
+    BinOp(BinOp),
 
+    // Keywords
+    Mutable,
+    Fn,
+    Public,
+    Internal,
+    Struct,
+    Union,
+
+    // Literals
+    IntLiteral(i64),
+    FloatLiteral(f64),
+    StringLiteral(String),
+    CharLiteral(char),
+    BoolLiteral(bool),
+    StructLiteral,
+    UnionLiteral,
+
+    // Identifiers
+    Identifier(String),
+
+    // Punctuation
+    OpenParen,
+    CloseParen,
+    OpenBrace,
+    CloseBrace,
+    OpenBracket,
+    CloseBracket,
+    Comma,
+    Colon,
+    Semicolon,
+    Dot,
+    QuestionMark,
+
+    Eof,
+}
+
+pub enum UnaryOp {
+    BitwiseNot,
+    Not,
+    Negate,
+    Plus,
+}
+
+pub enum BinOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    And,
+    Or,
+    Xor,
+    Equal,
+    NotEqual,
+    Less,
+    Greater,
+    LessEqual,
+    GreaterEqual,
 }
