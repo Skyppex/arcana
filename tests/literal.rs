7f3e02b6fdac99e35389c40add2bbf032820d301
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2024-05-14 16:43:24 +0200

    add tests for literal expression

---
+++ tests/literal.rs
@@ -0,0 +1,57 @@
+mod common;
+
+use common::{create_typed_ast, evaluate_expression, StatementExt, VecStatementExt};
+
+use shared::type_checker::ast::{Literal, TypedExpression};
+use interpreter::{value, Value};
+
+use crate::common::create_env;
+
+#[test]
+fn literal_is_literal() {
+    // Arrange
+    let input = "1";
+
+    // Act
+    let typed_ast = create_typed_ast(input);
+
+    // Assert
+    let expression = typed_ast.unwrap_program()
+        .nth_statement(0)
+        .unwrap_expression();
+
+    assert!(matches!(expression, TypedExpression::Literal { .. }));
+}
+
+#[test]
+fn literal_has_correct_type() {
+    // Arrange
+    let input = "1";
+
+    // Act
+    let typed_ast = create_typed_ast(input);
+
+    // Assert
+    let expression = typed_ast.unwrap_program()
+        .nth_statement(0)
+        .unwrap_expression();
+
+    match expression {
+        TypedExpression::Literal(literal) => {
+            assert!(matches!(literal, Literal::Int(_)));
+        },
+        _ => panic!("Expected a literal expression, but found {:?}", expression),
+    }
+}
+
+#[test]
+fn literal_returns_correct_value() {
+    // Arrange
+    let input = "1";
+    
+    // Act
+    let value = evaluate_expression(input, create_env(), false);
+
+    // Assert
+    assert_eq!(value, Value::Number(value::Number::Int(1)));
+}
