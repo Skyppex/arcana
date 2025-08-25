5cebb517d62261b3e4e388d81bf1a5f98044ae1c
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-08-25 16:28:34 +0200

    feat(nix): add checks, formatter, and a fix app to apply lint suggestions

--- flake.nix
+++ flake.nix
@@ -7,36 +7,86 @@
     naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
-        naerskLib = pkgs.callPackage naersk { };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    naersk,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      # system = "x86_64-linux";
+      pkgs = import nixpkgs {inherit system;};
+      naerskLib = pkgs.callPackage naersk {};
 
-        arcanaPackage = import ./default.nix {
-          naersk = naerskLib;
-          pkg-config = pkgs.pkg-config;
-        };
-      in {
-        # packages.default = arcanaPackage;
-        packages.default = naerskLib.buildPackage {
+      arcanaPackage = import ./default.nix {
+        naersk = naerskLib;
+        pkg-config = pkgs.pkg-config;
+      };
+    in {
+      # packages.default = arcanaPackage;
+      packages.default = naerskLib.buildPackage {
+        src = self;
+        nativeBuildInputs = [pkgs.pkg-config];
+      };
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [cargo rustc rustfmt clippy rust-analyzer];
+        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+
+      checks = {
+        cargo-check = naerskLib.buildPackage {
           src = self;
-          nativeBuildInputs = [ pkgs.pkg-config ];
+          mode = "check";
         };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ cargo rustc rustfmt clippy rust-analyzer ];
-          env.RUST_SRC_PATH =
-            "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        cargo-test = naerskLib.buildPackage {
+          src = self;
+          mode = "test";
         };
 
-        apps = rec {
-          mage = {
-            type = "app";
-            program = "${arcanaPackage}/bin/mage";
-          };
-          default = mage;
+        cargo-clippy = naerskLib.buildPackage {
+          src = self;
+          mode = "clippy";
         };
-      });
+
+        cargo-fmt =
+          pkgs.runCommand "cargo-fmt-check" {
+            buildInputs = [pkgs.rustfmt pkgs.cargo];
+          } ''
+            cp -r ${self} ./source
+            chmod -R +w ./source
+            cd ./source
+            cargo fmt --all -- --check
+            touch $out
+          '';
+      };
+
+      apps = rec {
+        default = mage;
+
+        mage = {
+          type = "app";
+          program = "${arcanaPackage}/bin/mage";
+        };
+
+        fix = {
+          type = "app";
+          program = "${pkgs.writeShellApplication {
+            name = "cargo-fix";
+            runtimeInputs = [pkgs.cargo pkgs.clippy];
+            text = ''
+              cargo clippy --fix --allow-dirty --allow-staged --all-targets
+            '';
+          }}/bin/cargo-fix";
+        };
+      };
+
+      formatter = pkgs.writeShellApplication {
+        name = "fmt";
+        runtimeInputs = [pkgs.rustfmt pkgs.cargo];
+        text = "cargo fmt --all";
+      };
+    });
 }
